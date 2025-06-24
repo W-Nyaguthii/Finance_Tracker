771c42c55587e83814c2b08ea05c3661f456d264
P2Wambui <wambuiprimrose0@gmail.com>
2025-06-24 15:11:51 +0300

    changes to homescreen

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        //  appBar: AppBar(),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed, //4+ icons
           items: const [
